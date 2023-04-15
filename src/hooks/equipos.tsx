b1aa94bc928c9dbe65c9b751c550762229932b5b
Pedro Chiappani <49994882+pedro-chiappani@users.noreply.github.com>
2023-04-15 16:16:05 -0300

    tablas de personas con equipos

---
+++ src/hooks/equipos.tsx
@@ -0,0 +1,95 @@
+
+export const equipos = [
+    {
+        equipo: "Timberwolves",
+        persona: "Joaco P",
+    },
+    {
+        equipo: "Hornets",
+        persona: "Agus",
+    },
+    {
+        equipo: "Hawks",
+        persona: "Nachito",
+    },
+    {
+        equipo: "Bulls",
+        persona: "Ayr",
+    },
+    {
+        equipo: "Mavericks",
+        persona: "Lucas",
+    },
+    {
+        equipo: "Jazz",
+        persona: "Yulo",
+    },
+    {
+        equipo: "76ers",
+        persona: "Santino",
+    },
+    {
+        equipo: "Heat",
+        persona: "Cocha",
+    },
+    {
+        equipo: "Bucks",
+        persona: "Pepe",
+    },
+    {
+        equipo: "Pelicans",
+        persona: "Gonza",
+    },
+    {
+        equipo: "Cavaliers",
+        persona: "Cafu",
+    },
+    {
+        equipo: "Grizzlies",
+        persona: "Juli",
+    },
+    {
+        equipo: "Celtics",
+        persona: "Laucha",
+    },
+    {
+        equipo: "Lakers",
+        persona: "Facu R",
+    },
+    {
+        equipo: "Warriors",
+        persona: "Pani",
+    },
+    {
+        equipo: "Suns",
+        persona: "Nico",
+    },
+    {
+        equipo: "Clippers",
+        persona: "San",
+    },
+    {
+        equipo: "Nuggets",
+        persona: "Joa",
+    },
+    {
+        equipo: "Nets",
+        persona: "Tom DC",
+    },
+    {
+        equipo: "Pistons",
+        persona: "Nacho K",
+    },
+    {
+        equipo: "Trail Blazers",
+        persona: "Agus M",
+    },
+    {
+        equipo: "Kings",
+        persona: "Lo",
+    },
+    {
+        equipo: "Knicks",
+        persona: "Qui",
+    },
+];
